dd76eb4dbad196f4c9af43e344463dbef3e796cd
Katrinagd <katrinaguyduy@gmail.com>
2019-07-28 20:11:22 -0700

    Added /login/ route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -28,7 +28,7 @@ router.post('/signup', async (req, res, next) => {
         const options = { expiresIn: '1 day' } //set expiration
         const token = jsonwebtoken.sign(payload, SECRET_KEY, options)
         // const token = generateToken(user._id)
-        res.status(status).json({ status, token })
+        res.status(status).json({ status, user, token })
     } catch(e) {
         console.error(e)
         const error = new Error(`Account can't be created`)
@@ -37,30 +37,30 @@ router.post('/signup', async (req, res, next) => {
     }
 })
 
-// router.post('/login', async (req, res, next) => {
-//     const { username, password } = req.body
-//     //find the user by username
-//     const guest = await Guest.findOne({ username })
-//     //if user exist, compare the plain text password to the hashed version
-//     if (guest) {
-//         const valid = await bcrypt.compare(password, guest.password)
-//         if (valid) {
-//         //if valid respond with success message
-//         const status = 200
-//         const response = 'You have successful logged in.'
-//         // Create a JWT
-//         const payload = { id: guest._id } //set up payload
-//         const options = { expiresIn: '1 day' } //set expiration
-//         const token = jsonwebtoken.sign(payload, SECRET_KEY, options)
-//         // const token = generateToken(guest._id)
-//         return res.status(status).json({ status, response, token })
-//         }
-//     }
+router.post('/login', async (req, res, next) => {
+    const { username, password } = req.body
+    //find the user by username
+    const user = await User.findOne({ username })
+    //if user exist, compare the plain text password to the hashed version
+    if (user) {
+        const valid = await bcrypt.compare(password, user.password)
+        if (valid) {
+        //if valid respond with success message
+        const status = 200
+        const response = 'You have successful logged in.'
+        // Create a JWT
+        const payload = { id: user._id } //set up payload
+        const options = { expiresIn: '1 day' } //set expiration
+        const token = jsonwebtoken.sign(payload, SECRET_KEY, options)
+        // const token = generateToken(guest._id)
+        return res.status(status).json({ status, response, token })
+        }
+    }
     
-//     const message = `Username or password incorrect. Please check credentials and try again.`
-//     const error = Error(message)
-//     error.status = 401
-//     next(error)
-// })
+    const message = `Username or password incorrect. Please check credentials and try again.`
+    const error = Error(message)
+    error.status = 401
+    next(error)
+})
 
 module.exports = router
