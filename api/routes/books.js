e567103d74118e624b7767b3ec49c0c7aa90c7a4
Jonathan Veilleux <wedge1126@gmail.com>
2019-07-30 23:33:36 -0700

    auth exercise

--- api/routes/books.js
+++ api/routes/books.js
@@ -1,5 +1,7 @@
+const { SECRET_KEY } = process.env
 const router = require('express').Router()
 const Book = require('../models/book')
+const jwt = require('jsonwebtoken')
 
 router.get('/', async (req, res, next) => {
   const status = 200
@@ -27,7 +29,14 @@ router.get('/:id', async (req, res, next) => {
 // You should only be able to create a book if the user is an admin
 router.post('/', async (req, res, next) => {
   const status = 200
+
   try {
+    const token = req.headers.authorization.split('Bearer ')[1]
+    const payload = jwt.verify(token, SECRET_KEY)
+    if(payload.admin === false) {
+      return next({ status: 401, message: 'Unauthorized.'})
+    } 
+
     const book = await Book.create(req.body)
     if (!book) throw new Error(`Request body failed: ${JSON.stringify(req.body)}`)
     
@@ -45,16 +54,31 @@ router.post('/', async (req, res, next) => {
 // You should only be able to reserve a book if a user is logged in
 router.patch('/:id/reserve', async (req, res, next) => {
   const { id } = req.params
+  let payload;
+  try {
+    const token = req.headers.authorization.split('Bearer ')[1]
+    payload = jwt.verify(token, SECRET_KEY)
+  } catch (e) {
+    return next({ status: 401, message: 'Invalid token.' })
+  }
+
   try {
     const book = await Book.findById(id)
     if (!book) {
       const error = new Error(`Invalid Book _id: ${id}`)
-      error.message = 404
+      error.status = 404
+      return next(error)
+    }
+
+    if(book.reserved.status === true) {
+      const error = new Error(`Book is already reserved.`)
+      error.status = 400
       return next(error)
     }
 
     book.reserved.status = true
     // Set the reserved memberId to the current user
+    book.reserved.memberId = payload.id
     await book.save()
     
     const response = await Book.findById(book._id).select('-__v')
@@ -68,11 +92,45 @@ router.patch('/:id/reserve', async (req, res, next) => {
 // You should only be able to return a book if the user is logged in
 // and that user is the one who reserved the book
 router.patch('/:id/return', async (req, res, next) => {
-  const status = 200
-  const message = 'You must implement this route!'
-  
-  console.log(message)
-  res.status(status).json({ status, message })
+  const { id } = req.params
+  let payload;
+  try {
+    const token = req.headers.authorization.split('Bearer ')[1]
+    payload = jwt.verify(token, SECRET_KEY)
+  } catch (e) {
+    return next({ status: 401, message: 'Invalid token.' })
+  }
+
+  try {
+    const book = await Book.findById(id)
+    if (!book) {
+      const error = new Error(`Invalid Book _id: ${id}`)
+      error.status = 404
+      return next(error)
+    }
+
+    if(book.reserved.status === false) {
+      const error = new Error(`Book is not reserved.`)
+      error.status = 400
+      return next(error)
+    }
+
+    if(!book.reserved.memberId.equals(payload.id)) {
+      const error = new Error(`Book is reserved by another user.`)
+      error.status = 401
+      return next(error)
+    }
+
+    book.reserved.status = false
+    book.reserved.memberId = null
+    await book.save()
+    
+    const response = await Book.findById(book._id).select('-__v')
+    const status = 200
+    res.json({ status, response })
+  } catch (e) {
+    console.error(e)
+  }
 })
 
 module.exports = router
